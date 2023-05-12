bc9f22b43e2756f55c01d4addecd37fe279ccd95
darker <darkerros@yandex.ru>
2023-05-12 21:27:01 +0300

    Revert "feat: complete queue page"
    This reverts commit db4f2637daab703517784a15ef839b324d2114e5.

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,62 +1,10 @@
-import {ChangeEventHandler, useState} from "react";
-import styles from './queue-page.module.css'
-import {SolutionLayout} from "../ui/solution-layout/solution-layout";
-import {Circle} from "../ui/circle/circle";
-import {ElementStates} from "../../types/element-states";
-import {Input} from "../ui/input/input";
-import {Button} from "../ui/button/button";
-import {useQueue} from "../../hooks/use-queue";
-
+import React from "react";
+import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const QueuePage: React.FC = () => {
-    const {queueElements,isQueueFull, isQueueEmpty, tailIndex, headIndex, dequeue, enqueue, clear} = useQueue(7)
+  return (
+    <SolutionLayout title="Очередь">
 
-    const [value, setValue] = useState<string>('')
-    const onChange:ChangeEventHandler<HTMLInputElement> = e => setValue(e.currentTarget.value)
-
-    const handleClickAdd = () => {
-        enqueue(value)
-        setValue('')
-    }
-    const handleClickDelete = () => {
-        dequeue()
-    }
-    const handleClickClear = () => {
-        clear()
-    }
-
-    return (
-        <SolutionLayout title="Очередь">
-            <div className={styles.btnContainer}>
-                <Input
-                    maxLength={4}
-                    isLimitText
-                    onChange={onChange}
-                    value={value}
-                    extraClass={styles.input}
-                />
-                <Button
-                    text="Добавить"
-                    type="button"
-                    onClick={handleClickAdd}
-                    disabled={isQueueFull || value === ""}
-                />
-                <Button
-                    text="Удалить"
-                    type="button"
-                    onClick={handleClickDelete}
-                    disabled={isQueueEmpty}
-                />
-                <Button
-                    text="Очистить"
-                    type="button"
-                    onClick={handleClickClear}
-                    disabled={isQueueEmpty}
-                />
-            </div>
-            <div className={styles.circleContainer}>
-                {queueElements.map((queElement, index) => <Circle key={index} index={index} letter={queElement} head={index === headIndex ? "Head" : ''} tail={index === tailIndex ? "Tail" : ''} state={ElementStates.Default}/>)}
-            </div>
-        </SolutionLayout>
-    );
+    </SolutionLayout>
+  );
 };
